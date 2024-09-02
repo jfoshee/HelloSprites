dfc51d09f8ce60df24de5e2146619e770bf30108
Jacob Foshee <jacobf@gmail.com>
2024-09-02 15:47:46 -0500

    select row in sprite sheet based on direction

--- Particle.cs
+++ Particle.cs
@@ -2,10 +2,12 @@ namespace HelloSprites;
 
 public sealed class Particle(Vector3 position, Vector3 velocity, float scale)
 {
+    private const int LifeSpanSeconds = 5;
+
     public Vector3 Position { get; set; } = position;
     public Vector3 Velocity { get; set; } = velocity;
     public float Scale { get; set; } = scale;
-    public TimeSpan Lifetime { get; private set; } = TimeSpan.FromSeconds(5);
+    public TimeSpan Lifetime { get; private set; } = TimeSpan.FromSeconds(LifeSpanSeconds);
     public bool Dead { get; private set; } = false;
     public int SpriteIndex { get; private set; }
 
@@ -23,9 +25,17 @@ public sealed class Particle(Vector3 position, Vector3 velocity, float scale)
         // Decrease the lifetime by the elapsed time
         Lifetime -= deltaTime;
 
+        // Choose the Row of the sprite-sheet based on the velocity
+        // 0 = Down, 1 = Right, 2 = Up, 3 = Left (flipped vertically from image)
+        int row = Math.Abs(Velocity.X) > Math.Abs(Velocity.Y)
+              ? (Velocity.X > 0 ? 1 : 3)
+              : (Velocity.Y > 0 ? 2 : 0);
+        var rowOffset = row * 4;
+
         // Update the particle's sprite index based on time
-        int totalFramesElapsed = (int)((5 - Lifetime.TotalSeconds) / FrameDuration.TotalSeconds);
-        SpriteIndex = SpriteIndices[totalFramesElapsed % SpriteIndices.Length];
+        var elapsedSeconds = LifeSpanSeconds - Lifetime.TotalSeconds;
+        int totalFramesElapsed = (int)(elapsedSeconds / FrameDuration.TotalSeconds);
+        SpriteIndex = SpriteIndices[totalFramesElapsed % SpriteIndices.Length] + rowOffset;
 
         // Check if the particle's lifetime has expired
         if (Lifetime <= TimeSpan.Zero)
